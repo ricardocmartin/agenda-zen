beb3a8db684ef7e1e68c39f73b192b6f6ff969a5
Ricardo Castro Martin <ricardo.cmartin@gmail.com>
2025-06-03 19:49:51 -0300

    Add modal form for clients and delete action

---
+++ src/components/Modal.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50" @click.self="$emit('close')">
+    <div class="bg-white p-6 rounded-lg shadow-lg w-full max-w-lg">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal'
+}
+</script>
