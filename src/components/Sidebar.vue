e984276b483149307a3654b43075ace699a3eba7
Ricardo Castro Martin <ricardo.cmartin@gmail.com>
2025-06-04 18:43:47 -0300

    Add user management page

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,6 +5,7 @@
         <router-link to="/dashboard" class="block text-gray-700 hover:text-blue-600">Início</router-link>
         <router-link to="/clientes" class="block text-gray-700 hover:text-blue-600">Clientes</router-link>
         <router-link to="/agendamentos" class="block text-gray-700 hover:text-blue-600">Agendamentos</router-link>
+        <router-link to="/usuarios" class="block text-gray-700 hover:text-blue-600">Usuários</router-link>
         <router-link to="/configuracao" class="block text-gray-700 hover:text-blue-600">Configurações</router-link>
       </nav>
     </aside>
