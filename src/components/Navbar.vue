5869a9752d3160bfb2876908412b113585731f7c
Ricardo Castro Martin <ricardo.cmartin@gmail.com>
2025-06-04 19:11:08 -0300

    feat: adicionar página pública de busca de perfis

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,9 @@
 <template>
     <nav class="flex items-center justify-between px-8 py-4 bg-white shadow">
         <div class="text-xl font-bold text-blue-600">Agenda Zen</div>
-        <div>
-        <router-link to="/login" class="text-blue-600 hover:underline mr-4">Login</router-link>
+        <div class="flex items-center space-x-4">
+        <router-link to="/buscar" class="text-blue-600 hover:underline">Buscar</router-link>
+        <router-link to="/login" class="text-blue-600 hover:underline">Login</router-link>
         <router-link to="/cadastro" class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">Cadastre-se</router-link>
         </div>
     </nav>
