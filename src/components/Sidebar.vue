95eac54615e878996ce328b8e22c60f1e88f95b6
Ricardo Castro Martin <ricardo.cmartin@gmail.com>
2025-06-04 19:47:37 -0300

    Make dashboard responsive

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,14 @@
 <template>
-    <aside class="w-64 bg-white shadow-md h-screen p-6">
+  <div>
+    <div
+      v-if="isOpen"
+      class="fixed inset-0 bg-black bg-opacity-50 z-40 md:hidden"
+      @click="$emit('close')"
+    ></div>
+    <aside
+      class="fixed inset-y-0 left-0 transform bg-white shadow-md w-64 h-screen p-6 transition-transform duration-200 z-50 md:relative md:translate-x-0"
+      :class="{ '-translate-x-full md:translate-x-0': !isOpen }"
+    >
       <h2 class="text-2xl font-bold text-blue-600 mb-6">Agenda Zen</h2>
       <nav class="space-y-4">
         <router-link to="/dashboard" class="block text-gray-700 hover:text-blue-600">Início</router-link>
@@ -10,10 +19,17 @@
         <router-link to="/configuracao" class="block text-gray-700 hover:text-blue-600">Configurações</router-link>
       </nav>
     </aside>
-  </template>
+  </div>
+</template>
   
-  <script>
-  export default {
-    name: 'Sidebar'
+<script>
+export default {
+  name: 'Sidebar',
+  props: {
+    isOpen: {
+      type: Boolean,
+      default: true
+    }
   }
-  </script>
+}
+</script>
