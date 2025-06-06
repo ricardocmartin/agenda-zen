5926df1bff10ce7459d06a80d540b0468bfeb163
Ricardo Castro Martin <ricardo.cmartin@gmail.com>
2025-06-06 19:11:58 -0300

    Add submenu in settings for profile and agenda

---
+++ supabase/schemas/008_add_schedule_to_profiles.sql
@@ -0,0 +1,3 @@
+alter table profiles add column if not exists start_time time;
+alter table profiles add column if not exists end_time time;
+alter table profiles add column if not exists week_days text;
