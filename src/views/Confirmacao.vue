22c9560a092084d88f18f36d13274c81dc376cd7
Jeferson Carlos <jeferson.carlos@hotmail.com>
2025-06-18 16:55:38 -0300

    Add Login button on confirmation page

--- src/views/Confirmacao.vue
+++ src/views/Confirmacao.vue
@@ -4,7 +4,10 @@
     <div class="bg-white p-10 rounded-2xl shadow-xl w-full max-w-lg text-center space-y-6">
       <img src="/icons/check.svg" alt="Sucesso" class="w-16 h-16 mx-auto text-green-500" />
       <h1 class="text-2xl font-bold text-green-600">Cadastro efetuado com sucesso</h1>
-      <router-link to="/" class="btn inline-block">Início</router-link>
+      <div class="flex justify-center space-x-4">
+        <router-link to="/" class="btn inline-block">Início</router-link>
+        <router-link to="/login" class="btn inline-block">Login</router-link>
+      </div>
     </div>
   </section>
   <Footer />
