25cd5fddd85c7ba6287fc543701a9a9e31b7cbca
Jeferson Carlos <jeferson.carlos@hotmail.com>
2025-06-18 09:32:09 -0300

    Make client form responsive with scroll

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50" @click.self="$emit('close')">
-    <div class="bg-white p-6 rounded-lg shadow-lg w-full max-w-lg">
+    <div class="bg-white p-6 rounded-lg shadow-lg w-full max-w-lg md:max-w-2xl max-h-[90vh] overflow-y-auto">
       <slot></slot>
     </div>
   </div>
