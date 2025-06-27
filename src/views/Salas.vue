34be3492674699394176e7d2e1d9db9be6be834d
Jeferson Carlos <jeferson.carlos@hotmail.com>
2025-06-27 16:19:46 -0300

    fix merge conflicts in Salas.vue and update view/edit buttons

--- src/views/Salas.vue
+++ src/views/Salas.vue
@@ -22,7 +22,7 @@
               class="border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
             <button
-              @click="openModal"
+              @click="openModal()"
               class="btn"
             >
               Nova Sala
